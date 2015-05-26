485f6e7dafa02e94bcc3212e8611bb2e8094ad7a
chao.zhang <chao.zhang@dianping.com>
2015-05-26 10:57:41 +0800

    发布脚本修改 and 删除xml配置文件

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env bash
-
 ## 1. 拉取远程包
 ## 2. 解压包
 ## 3. rsync
 
-WORMHOLE_ROOT="/data/deploy/chao.zhang/wormhole-zebra/"
+WORMHOLE_ROOT="/data/deploy/chao.zhang/wormhole-$1/"
 JAR_NAME="nebula-wormhole-product-0.2.0-SNAPSHOT.jar"
 FTP_DIR="http://10.1.1.81/product-datadp-wormhole/"
-
-if [ "$1" != "" ];then
+if [ "$2" != "" ];then
     rm -rf $WORMHOLE_ROOT
     mkdir -p $WORMHOLE_ROOT
     cd $WORMHOLE_ROOT
-    rm -rf *
-    wget -O "./$JAR_NAME" "$FTP_DIR$1/$JAR_NAME"
+    wget -O "./$JAR_NAME" "$FTP_DIR$2/$JAR_NAME"
     jar -xvf $JAR_NAME
-
     src=$WORMHOLE_ROOT
     tgt=$WORMHOLE_ROOT
     OUTPUT_LOG=/tmp/output-$$.log
     ERROR_LOG=/tmp/error-$$.log
     MACHINES=("10.1.6.49" "10.2.2.22" "10.2.2.23" "10.1.110.49" "10.1.115.31" "10.1.115.32" "10.2.2.86" "10.2.2.87" "10.1.110.94" "10.1.110.96")
-
     for machine in ${MACHINES[@]}
     do
         echo -e "rsync --delete -av -e \"ssh -p58422\" $src deploy@$machine:$tgt" | sh
