728b6abfef9614b41d9f2cc4c8c3c9eaec0cc48a
zcfrank1st <zcfrank1st@sina.com>
2015-07-05 09:52:33 +0800

    执行脚本修改

--- bin/runtask.sh
+++ bin/runtask.sh
@@ -5,7 +5,24 @@ CURR_DIR=`pwd`
 cd `dirname "$0"`/..
 WORMHOLE_HOME=`pwd`
 
-CMD="sh ${WORMHOLE_HOME}/bin/timemachine.sh $1"
-CMD="$CMD;sh ${WORMHOLE_HOME}/bin/wormhole.sh $1"
-echo $CMD
-eval $CMD
+if [ -z "$1" ]
+then
+    echo "usage: sh runtask.sh [path : file absolute path]"
+else
+    TIMEMACHINE="sh ${WORMHOLE_HOME}/bin/timemachine.sh $1"
+    echo $TIMEMACHINE
+    eval $TIMEMACHINE
+    echo "starting to run timemachine..."
+    if [ "$?" -ne 0 ];then
+        echo "timemachine exec --- failed!"
+        exit
+    fi
+    WORMHOLE="sh ${WORMHOLE_HOME}/bin/wormhole.sh $1"
+    echo $WORMHOLE
+    eval $WORMHOLE
+    echo "starting to run wormhole..."
+    if [ "$?" -ne 0 ];then
+        echo "wormhole exec --- failed!"
+        exit
+    fi
+fi
